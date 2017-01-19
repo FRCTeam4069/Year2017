3b45a2d6d1b955d3107168829b9e9217dd6f039d
Eric Albers <eric@ericalbers.com>
2017-01-19 13:27:59 -0500

    cleanup, we use ALLMAN style braces/formatting

--- FRC4069_2017Code/src/org/usfirst/frc/team4069/robot/IOMapping.java
+++ FRC4069_2017Code/src/org/usfirst/frc/team4069/robot/IOMapping.java
@@ -10,43 +10,43 @@ import edu.wpi.first.wpilibj.Joystick;
  */
 public interface IOMapping
 {
-  
+
   // Drive motor PWM ports
   public static final int LEFT_DRIVE_MOTOR_PWM_PORT = 0;
   public static final int RIGHT_DRIVE_MOTOR_PWM_PORT = 1;
-  
+
   // Left and right arm canbus ports
   public static final int LEFT_ARM_CANBUS_PORT = 2;
   public static final int RIGHT_ARM_CANBUS_PORT = 1;
-  
+
   // Shooter PWM port
   public static final int SHOOTER_PWM_PORT = 5;
-  
+
   // Roller wheels PWM port
   public static final int ROLLERWHEELS_PWM_PORT = 6;
-  
+
   // Intake PWM port
   public static final int INTAKE_PWM_PORT = 4;
-  
+
   // Shooter encoder digital IO ports
   public static final int SHOOTER_ENCODER_DIO_1 = 4;
   public static final int SHOOTER_ENCODER_DIO_2 = 5;
-  
+
   // Intake sensor digital IO
   public static final int INTAKE_SENSOR_DIO = 8;
-  
+
   // Ready to shoot sensor digital IO
   public static final int READY_TO_SHOOT_DIO = 6;
-  
+
   // Arm digital IO
   public static final int ARM_DIO = 7;
-  
+
   // Compressor port
   public static final int COMPRESSOR_PORT = 0;
-  
+
   // Compressor digital IO
   public static final int COMPRESSOR_DIO = 9;
-  
+
   /**
    * Left encoder Digital I/O ports.
    */
@@ -57,7 +57,7 @@ public interface IOMapping
    */
   public static final int RIGHT_DRIVE_ENCODER_1 = 2;
   public static final int RIGHT_DRIVE_ENCODER_2 = 3;
-  
+
   /**
    * Drive joystick port.
    */
@@ -67,18 +67,16 @@ public interface IOMapping
    */
   public static final int CONTROL_JOYSTICK_PORT = 1;
 
-public static final int DRIVER_Y_BUTTON = 4;
-public static final int DRIVER_A_BUTTON = 1;
-public static final int DRIVER_X_BUTTON = 3;
-public static final int DRIVER_B_BUTTON = 2;
-public static final int DRIVER_RB_BUTTON = 6;
+  public static final int DRIVER_Y_BUTTON = 4;
+  public static final int DRIVER_A_BUTTON = 1;
+  public static final int DRIVER_X_BUTTON = 3;
+  public static final int DRIVER_B_BUTTON = 2;
+  public static final int DRIVER_RB_BUTTON = 6;
 
-  
- public static final int CONTROL_Y_BUTTON = 4;
- public static final int CONTROL_A_BUTTON = 1;
- public static final int CONTROL_X_BUTTON = 3;
- public static final int CONTROL_B_BUTTON = 2;
- public static final int CONTROL_RB_BUTTON = 6;
+  public static final int CONTROL_Y_BUTTON = 4;
+  public static final int CONTROL_A_BUTTON = 1;
+  public static final int CONTROL_X_BUTTON = 3;
+  public static final int CONTROL_B_BUTTON = 2;
+  public static final int CONTROL_RB_BUTTON = 6;
 
- 
 }
--- Tank Drive/src/org/usfirst/frc/team4069/robot/Robot.java
+++ Tank Drive/src/org/usfirst/frc/team4069/robot/Robot.java
@@ -20,25 +20,29 @@ import edu.wpi.first.wpilibj.Timer;
  * be much more difficult under this system. Use IterativeRobot or Command-Based
  * instead if you're new.
  */
-public class Robot extends SampleRobot {
-	RobotDrive myRobot = new RobotDrive(0, 1); // class that handles basic drive
-												// operations
-	Joystick leftStick = new Joystick(0); // set to ID 1 in DriverStation
-	Joystick rightStick = new Joystick(1); // set to ID 2 in DriverStation
+public class Robot extends SampleRobot
+{
+  RobotDrive myRobot = new RobotDrive(0, 1); // class that handles basic drive
+  // operations
+  Joystick leftStick = new Joystick(0); // set to ID 1 in DriverStation
+  Joystick rightStick = new Joystick(1); // set to ID 2 in DriverStation
 
-	public Robot() {
-		myRobot.setExpiration(0.1);
-	}
+  public Robot()
+  {
+    myRobot.setExpiration(0.1);
+  }
 
-	/**
-	 * Runs the motors with tank steering.
-	 */
-	@Override
-	public void operatorControl() {
-		myRobot.setSafetyEnabled(true);
-		while (isOperatorControl() && isEnabled()) {
-			myRobot.tankDrive(leftStick, rightStick);
-			Timer.delay(0.005); // wait for a motor update time
-		}
-	}
+  /**
+   * Runs the motors with tank steering.
+   */
+  @Override
+  public void operatorControl()
+  {
+    myRobot.setSafetyEnabled(true);
+    while (isOperatorControl() && isEnabled())
+    {
+      myRobot.tankDrive(leftStick, rightStick);
+      Timer.delay(0.005); // wait for a motor update time
+    }
+  }
 }
