439d23b905567143710585f3f3903892c526501d
Brendon Matusch <brendon-m@outlook.com>
2017-01-21 13:32:51 -0500

    initial setup of motion recorder

---
+++ RecordPlayback/org/usfirst/frc/team4069/RecordMotions.java
@@ -0,0 +1,22 @@
+package org.usfirst.frc.team4069;
+
+import org.usfirst.frc.team4069.robot.Robot;
+
+public class RecordMotions {
+
+	
+	private Robot robot = null;
+	private ArrayList<double> leftWheelPositions = new ArrayList<double>();
+	private ArrayList<double> rightWheelPositions = new ArrayList<double>();
+  
+	public RecordMotions(Robot robot)
+	{
+		this.robot = robot;
+	}
+	
+	private void RecordActions () {
+		// THIS METHOD IS NOT CALLED: IT MUST BE UPDATED TO WORK WITH THE FRC LIBRARIES
+		leftWh
+	}
+	  
+}
