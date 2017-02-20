fb07a18b400bb741a6a266a6f4c9483e7bd558a1
Troy Laakso <tlaakso@eastlink.ca>
2017-02-20 15:22:15 -0500

    Updated IOMapping

--- FRC4069_2017Code/src/org/usfirst/frc/team4069/robot/IOMapping.java
+++ FRC4069_2017Code/src/org/usfirst/frc/team4069/robot/IOMapping.java
@@ -11,18 +11,18 @@ import edu.wpi.first.wpilibj.Joystick;
 public class IOMapping
 {
   // Drive motor PWM ports
-  public static final int LEFT_DRIVE_MOTOR_PWM_PORT = 0;
-  public static final int RIGHT_DRIVE_MOTOR_PWM_PORT = 1;
+  public static final int LEFT_DRIVE_MOTOR_PWM_PORT = 2;
+  public static final int RIGHT_DRIVE_MOTOR_PWM_PORT = 4;
 
   public static final int SHOOTER_CANBUS_PORT = 0;
   public static final int TURRET_CANBUS_PORT = 1;
 
-  public static final int WINCH_PWN_PORT = 9;
-  public static final int INTAKE_FRONT_PWM_PORT = 8;
-  public static final int INTAKE_BACK_PWM_PORT = 7;
-  public static final int ELEVATOR_PWM_PORT = 6;
+  public static final int WINCH_PWN_PORT = 6;
+  public static final int INTAKE_FRONT_PWM_PORT = 7;
+  public static final int INTAKE_BACK_PWM_PORT = 5;
+  public static final int ELEVATOR_PWM_PORT = 9;
   
-  public static final int FEED_PWM_PORT = 10;
+  public static final int FEED_PWM_PORT = 8;
   
   public static final int TURRET_LIMIT_SWITCH = 8;
   
@@ -54,7 +54,11 @@ public class IOMapping
   public static final int DRIVER_RIGHT_BACK_BUTTON = 6;
   public static final int DRIVER_SMALL_BACK_BUTTON = 7; //on front of controller, small black button labeled back
   public static final int DRIVER_SMALL_START_BUTTON = 8; //on front of controller small black start button
-
+  public static final int DRIVER_RIGHT_Y_AXIS = 5;
+  public static final int DRIVER_RIGHT_X_AXIS = 4;
+  public static final int DRIVER_LEFT_Y_AXIS = 1;
+  public static final int DRIVER_LEFT_X_AXIS = 0;
+  
   public static final int CONTROL_Y_BUTTON = 4;
   public static final int CONTROL_A_BUTTON = 1;
   public static final int CONTROL_X_BUTTON = 3;
@@ -63,7 +67,10 @@ public class IOMapping
   public static final int CONTROL_RIGHT_BACK_BUTTON = 6;
   public static final int CONTROL_SMALL_BACK_BUTTON = 7; //small black button on front of controller labeled 'back'
   public static final int CONTROL_SMALL_START_BUTTON = 8; //small black button on front of controller labeled start
-
+  public static final int CONTROL_RIGHT_Y_AXIS = 5;
+  public static final int CONTROL_RIGHT_X_AXIS = 4;
+  public static final int CONTROL_LEFT_Y_AXIS = 1;
+  public static final int CONTROL_LEFT_X_AXIS = 0;
   
   
   
