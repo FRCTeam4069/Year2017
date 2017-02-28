d913c27b5cc25a8098b3190e675ff15c52dce7d2
Troy Laakso <tlaakso@eastlink.ca>
2017-02-28 18:32:36 -0500

    Updated IOMapping ports

--- FRC4069_2017Code/src/org/usfirst/frc/team4069/robot/IOMapping.java
+++ FRC4069_2017Code/src/org/usfirst/frc/team4069/robot/IOMapping.java
@@ -8,18 +8,20 @@ import edu.wpi.first.wpilibj.Joystick;
 public class IOMapping
 {
   // Drive motor PWM ports
-  public static final int LEFT_DRIVE_MOTOR_PWM_PORT = 2;
-  public static final int RIGHT_DRIVE_MOTOR_PWM_PORT = 4;
-
+  public static final int LEFT_DRIVE_MOTOR_PWM_PORT = 0; // red
+  public static final int RIGHT_DRIVE_MOTOR_PWM_PORT = 1; // green
+  
+  public static final int ELEVATOR_PWM_PORT = 2; // green/blue band
+  
+  public static final int FEED_PWM_PORT = 3; // yellow
+  
+  public static final int INTAKE_BACK_PWM_PORT = 4;
+  public static final int INTAKE_FRONT_PWM_PORT = 5;
+  
+  public static final int WINCH_PWN_PORT = 6; // blue
+  
   public static final int SHOOTER_CANBUS_PORT = 0;
   public static final int TURRET_CANBUS_PORT = 1;
-
-  public static final int WINCH_PWN_PORT = 6;
-  public static final int INTAKE_FRONT_PWM_PORT = 7;
-  public static final int INTAKE_BACK_PWM_PORT = 5;
-  public static final int ELEVATOR_PWM_PORT = 3;
-
-  public static final int FEED_PWM_PORT = 8;
 
   public static final int TURRET_LIMIT_SWITCH = 8;
 
